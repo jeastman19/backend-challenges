6736dade124133bbde932526c8c967317704d10b
Jorge Eastman <jeastman19@gmail.com>
2024-07-03 16:03:06 -0300

    The user repository is renamed

---
+++ src/infra/repositories/user-repository-impl.ts
@@ -0,0 +1,25 @@
+import { Role } from '@src/domain/entities/role';
+
+import { User } from '../../domain/entities/user';
+import { UserRepository } from '../../domain/repositories/user-repository';
+import UserModel from '../models/user-model';
+
+export class UserRepositoryImpl implements UserRepository {
+    async save(user: User): Promise<User> {
+        const userDoc = new UserModel({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            role: user.role,
+        });
+        const savedUser = await userDoc.save();
+        return User.create(
+            savedUser.id,
+            savedUser.name,
+            savedUser.email,
+            savedUser.password,
+            savedUser.role as Role,
+        );
+    }
+}
